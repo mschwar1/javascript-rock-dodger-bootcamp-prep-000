81bd01bf0643623bc351295edc9c07200a9367bf
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-22 01:11:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,13 +22,15 @@ function checkCollision(rock) {
     const rockLeftEdge = positionToInteger(rock.style.left);
     const rockRightEdge = rockLeftEdge + 20;
 
-    return (
+    if (
     (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
  
     (rockLeftEdge >= dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
              
-    (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)); 
-    } 
+    (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)) { return true} 
+    } else {
+      return false}
+    }
   }
 
 function createRock(x) {

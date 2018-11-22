188231c6b878685108482d2e6be22407e3a3941d
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-22 01:19:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function endGame() {
   
   clearInterval(gameInterval);
 
-  ROCKS.forEach(function(rock) {rock.remove})
+  ROCKS.forEach(function(rock) {rock.remove()});
 
   window.removeEventListener('keydown', moveDodger);
   alert('YOU LOSE!');
